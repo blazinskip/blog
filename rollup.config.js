8b495fd6760a122af998af62906efe5b0b065c47
blazinskip <blazinski.patryk@gmail.com>
2020-06-02 18:05:01 +0200

    refactor: 💡 cleanup project structure

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,37 @@
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
-import commonjs from '@rollup/plugin-commonjs';
-import svelte from 'rollup-plugin-svelte';
-import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import config from 'sapper/config/rollup.js';
-import pkg from './package.json';
+import alias from '@rollup/plugin-alias'
+import resolve from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+import commonjs from '@rollup/plugin-commonjs'
+import svelte from 'rollup-plugin-svelte'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import autoPreprocess from 'svelte-preprocess'
+import config from 'sapper/config/rollup.js'
+import path from 'path'
+import pkg from './package.json'
 
-const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
-const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+const mode = process.env.NODE_ENV
+const dev = mode === 'development'
+const legacy = !!process.env.SAPPER_LEGACY_BUILD
+const aliases = {
+	'@': path.resolve(__dirname, 'src')
+}
 
-const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
+const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning)
 
 export default {
 	client: {
 		input: config.client.input(),
 		output: config.client.output(),
 		plugins: [
+			alias({ entries: aliases }),
 			replace({
 				'process.browser': true,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
 			svelte({
 				dev,
+				preprocess: autoPreprocess(),
 				hydratable: true,
 				emitCss: true
 			}),
@@ -35,7 +43,7 @@ export default {
 
 			legacy && babel({
 				extensions: ['.js', '.mjs', '.html', '.svelte'],
-				babelHelpers: 'runtime',
+				runtimeHelpers: true,
 				exclude: ['node_modules/@babel/**'],
 				presets: [
 					['@babel/preset-env', {
@@ -55,7 +63,6 @@ export default {
 			})
 		],
 
-		preserveEntrySignatures: false,
 		onwarn,
 	},
 
@@ -63,6 +70,7 @@ export default {
 		input: config.server.input(),
 		output: config.server.output(),
 		plugins: [
+			alias({ entries: aliases }),
 			replace({
 				'process.browser': false,
 				'process.env.NODE_ENV': JSON.stringify(mode)
@@ -80,7 +88,6 @@ export default {
 			require('module').builtinModules || Object.keys(process.binding('natives'))
 		),
 
-		preserveEntrySignatures: 'strict',
 		onwarn,
 	},
 
@@ -97,7 +104,6 @@ export default {
 			!dev && terser()
 		],
 
-		preserveEntrySignatures: false,
 		onwarn,
 	}
-};
+}
