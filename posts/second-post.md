c2a299180c3e0686f087815e6267a8abacd2fe97
blazinskip <blazinski.patryk@gmail.com>
2020-06-02 18:46:48 +0200

    feat: 🎸 add post page

---
+++ posts/second-post.md
@@ -0,0 +1,21 @@
+---
+title: Second Post
+summary: A short post about ...
+date: 2020-03-01
+---
+
+# Second Post
+
+## Getting Started
+
+This is content for getting started
+
+- this
+- is
+- markdown
+
+    
+   ```javascript
+  const java = 'Java';
+   console.log(java);
+```
---
+++ src/routes/posts/[permalink].svelte
@@ -0,0 +1,24 @@
+<script context="module">
+  // import the logic for finding a post based on permalink
+  import {findPost} from '../../posts'
+
+  // sapper calls this to load our data
+  export function preload(page) {
+    // find the post based on the permalink param
+    const post = findPost(page.params.permalink)
+
+    // return a list of props
+    return { post }
+  }
+</script>
+
+<script>
+  // this prop is filled from the result of the `preload()`
+  export let post;
+</script>
+
+<svelte:head>
+  <title>{post.title}</title>
+</svelte:head>
+
+{@html post.html}
