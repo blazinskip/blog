82d0eed53d8a05a09050ad3faee0cc6855841437
blazinskip <blazinski.patryk@gmail.com>
2020-06-05 19:09:09 +0200

    feat: 🎸 start working on navbar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,13 +2,44 @@
   export let segment;
 </script>
 
+<style>
+  .header {
+    align-items: center;
+    justify-content: space-between;
+    display: flex;
+    height: 4rem;
+  }
 
-<header role="banner">
-  💻 Patric Blazinski
+  .header__title.title {
+    margin-bottom: 0;
+    height: 98%;
+  }
+
+  .title__link {
+    display: flex;
+    height: 98%;
+    padding: 0 1rem;
+    text-decoration: none;
+    align-items: center;
+  }
+
+  .header__links.links {
+    flex-direction: row;
+    display: flex;
+    padding-left: 0;
+    list-style: none;
+  }
+</style>
+
+
+<header class="header" role="banner">
+  <h1 class="header__title title">
+    <a class="title__link" aria-current="{segment === undefined ? 'page' : undefined}" href=".">💻 Patryk Blazinski</a>
+  </h1>
+
 
   <nav>
-    <ul>
-      <li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
+    <ul class="header__links links">
       <li><a aria-current="{segment === undefined ? 'page' : undefined}" href="/about">About</a></li>
       <li><a aria-current="{segment === undefined ? 'page' : undefined}" href="/contact">contact</a></li>
     </ul>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -5,6 +5,13 @@
 </script>
 
 <style>
+	.blog {
+		margin-left: auto;
+		margin-right: auto;
+		max-width: 50rem;
+		padding: 2.625rem 1.3125rem;
+	}
+
 	main {
 		position: relative;
 		max-width: 56em;
@@ -15,8 +22,10 @@
 	}
 </style>
 
-<Nav {segment}></Nav>
+<div class="blog">
+	<Nav {segment}></Nav>
 
-<main>
-	<slot></slot>
-</main>
+	<main>
+		<slot></slot>
+	</main>
+</div>
