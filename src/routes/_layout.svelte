be46d36bf719a4e7863f21a0551f322a8fb4aa92
pblazinski <pblazinski12013@gmail.com>
2020-06-17 16:46:58 +0200

    feat: 🎸 remove max-width wrapper from blog

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,18 +1,9 @@
 <style>
   .blog {
     position: relative;
-    margin-left: auto;
-    margin-right: auto;
-    max-width: 50rem;
-    padding: 2.625rem 1.3125rem;
   }
 
-  main {
-    max-width: 56em;
-    background-color: white;
-    padding: 2em;
-    margin: 0 auto;
-    box-sizing: border-box;
+  .blog__content {
   }
 </style>
 
@@ -25,7 +16,7 @@
 <div class="blog">
   <Nav {segment} />
 
-  <main>
+  <main class="blog__content">
     <slot />
   </main>
 </div>
