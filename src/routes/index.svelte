54f43b33ef3998a2f711e7f58658596dc3278be6
blazinskip <blazinski.patryk@gmail.com>
2020-07-11 17:03:34 +0200

    feat: 🎸 update layout at index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,26 +1,5 @@
 <style>
-  h1,
-  p {
-    text-align: center;
-    margin: 0 auto;
-  }
 
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
-  }
 </style>
 
 <script>
@@ -28,18 +7,18 @@
 </script>
 
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>Patryk Blazinski</title>
 </svelte:head>
 
-<h1>My weblog</h1>
-
-{#each posts as post}
-  <article>
-    <!-- link article to /posts/$permalink -->
-    <a href="{`/posts/${post.permalink}`}">
-      <h2>{post.title}</h2>
-      <span>{post.readInformation.humanized}</span>
-      <p>{post.summary}</p>
-    </a>
-  </article>
-{/each}
+<div class="container">
+  {#each posts as post}
+    <article>
+      <!-- link article to /posts/$permalink -->
+      <a href="{`/posts/${post.permalink}`}">
+        <h2>{post.title}</h2>
+        <span>{post.readInformation.humanized}</span>
+        <p>{post.summary}</p>
+      </a>
+    </article>
+  {/each}
+</div>
