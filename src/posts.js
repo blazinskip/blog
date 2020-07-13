578361e7f4f3e65d3b0ed3777f5c6f3816841010
blazinskip <blazinski.patryk@gmail.com>
2020-07-13 16:20:08 +0200

    feat: 🎸 add date-fns for post date format

--- src/posts.js
+++ src/posts.js
@@ -1,5 +1,6 @@
 import all from '../posts/*.md';
 import timeToReadArticle from './utils/time-to-read-article';
+import { format } from 'date-fns';
 
 export const posts = all
   .filter(({ metadata }) => metadata.published)
@@ -16,7 +17,7 @@ export const categories = Array.from(new Set(allCategories));
 function transform({ filename, html, metadata }) {
   // TODO: rename permalink to link
   const permalink = filename.replace(/\.md$/, '');
-  const date = new Date(metadata.date);
+  const date = format(new Date(metadata.date), 'MMMM dd, yyyy');
   const readInformation = timeToReadArticle(html);
 
   return { ...metadata, filename, html, permalink, date, readInformation };
