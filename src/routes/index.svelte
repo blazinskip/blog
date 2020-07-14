d6444b75adff029f1987eaa1d24dcb2b9b87ec01
blazinskip <blazinski.patryk@gmail.com>
2020-07-14 15:22:04 +0200

    feat: 🎸 update blog list at home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,36 @@
-<style>
+<style lang="scss">
+  .post {
+    .post__date {
+      margin-bottom: var(--space-xs);
+    }
 
+    .post__link {
+      margin: calc(-1 * var(--space-xs));
+      padding: var(--space-xs);
+      display: block;
+
+      transition: background 0.1s ease;
+
+      &:hover {
+        background: rgba(#849eff, 0.15);
+      }
+    }
+
+    .post__title {
+      margin: 0 0 var(--space-xs);
+    }
+
+    .post__summary {
+      margin-top: 0;
+      margin-bottom: 0;
+    }
+  }
+
+  .separator {
+    border: 1px solid rgba(#333, 0.15);
+    border-bottom: none;
+    margin: var(--space-lg) 0;
+  }
 </style>
 
 <script>
@@ -14,12 +45,14 @@
   {#each posts as post}
     <div class="post">
       <!-- link article to /posts/$permalink -->
-      <a href="{`/posts/${post.permalink}`}">
-        <span>{post.date}</span>
-        <h2>{post.title}</h2>
-        <span>{post.readInformation.humanized}</span>
-        <p>{post.summary}</p>
+      <a class="post__link" href="{`/posts/${post.permalink}`}">
+        <div class="post__date">{post.date}</div>
+
+        <h2 class="post__title">{post.title}</h2>
+        <p class="post__summary">{post.summary}</p>
       </a>
     </div>
+
+    <hr class="separator" />
   {/each}
 </div>
